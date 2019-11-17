3ca2a6693a8736d7f0998b215c69982457a02621
Serina Harris <sbharris1977@gmail.com>
2019-11-17 00:01:10 -0600

    final commit for homework changes

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # My-Weather-App
+
+I found this to be challenging. I was not able to retrieve the different cities with a search function.
+But after brainstorming I decided to create links to cities that are already listed. The cities will have
+to be clicked in the order in which they appear to work.
+
+I was happy that I was at least able to pull data from the API because earlier this week I wasn't able to do so.
+I followed Bob's advice. If you can;t make it work, then do what you know how to do.
--- script.js
+++ script.js
@@ -1,90 +1,118 @@
-//select elements created in html
-/*const notifyElement = document.querySelector('.notification');
-const locationEl = document.querySelector('.location p');
-const icon = document.querySelector('.weather-icon');
-const tempEl = document.querySelector('.temperature-value p');
-const humidEl = document.querySelector('.humidity-value p');
-const windElement = document.querySelector('.wind-speed p');
-const uvElement = document.querySelector('.uv-index p');
-
-//add data
-const weather = {};
-//convert kelvin into celsius and/or farenhiet
-weather.temperature = {
-    unit: 'celsius'
-}
-
-const KELVIN = 273;
-
+//Javascript
 //API Key 
-const APIKey = "1dd029263e9a36200e5256b3f8ab4a28";
-
-//Check to see if browser supports Gelocation
-if('geolocation' in navigator){
-    navigator.geolocation.getCurrentPosition(setPosition, showError);
-    }else{
-        notifyElement.style.display = 'block';
-        notifyElement.innerHTML = "<p>Browser doesn't support Geolocation</p>";
-    }
-
-    //Get the uers location/position
-    function setPosition(postion){
-let latitude = position.coords.latitude;
-let longitude = position.coords.longitude;
-
-getWeather(latitude, longitude);
-
-    }
-
-    //Show the error if geolocation doesn't work
-    function showError(error){
-        notifyElement.style.display = 'block';
-        notifyElement.innerHTML = '<p> ${error.message} </p>';
-
-    }*/
-
-    //API Key 
 var APIKey = "1dd029263e9a36200e5256b3f8ab4a28";
 
-    //get weather from API
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +"q=Austin&units=imperial&appid=" + APIKey;
-     
-    
+//get weather from API for current day
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?" + "q=Austin&units=imperial&appid=" + APIKey;
 
-  // Here we run our AJAX call to the OpenWeatherMap API
+
+
+// Here we run our AJAX call to the OpenWeatherMap API
+$.ajax({
+  url: queryURL,
+  method: "GET"
+})
+  // We store all of the retrieved data inside of an object called "response"
+  .then(function (response) {
+
+    //Log the queryURL
+    //console.log(queryURL)
+
+
+    // Log the resulting object
+    //console.log(response)
+
+    // Transfer content from API to HTML
+    $(".location").html("<p>" + response.name);
+    $(".weather-icon").html("<p>" + response.weather[0].icon);
+    $(".temperature-value").html("Temp: " + response.main.temp + "°F");
+    $(".humidity-value").html("Humidity: " + response.main.humidity + "%");
+    $(".wind-speed").html("Wind Speed: " + response.wind.speed + " MPH");
+  })
+
+///Atlanta
+// var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +"q=Atlantan&units=imperial&appid=" + APIKey;
+
+$(".atlanta").on("click", event);
+
+function event() {
+  var queryURL = "https://api.openweathermap.org/data/2.5/weather?" + "q=Atlanta&units=imperial&appid=" + APIKey;
   $.ajax({
     url: queryURL,
     method: "GET"
   })
-    // We store all of the retrieved data inside of an object called "response"
-    .then(function(response) {
+    .then(function (response) {
 
-      // Log the queryURL
-      console.log(queryURL);
-
-      // Log the resulting object
-      console.log(response);
-
-      //Transfer content from API to HTML
       $(".location").html("<p>" + response.name);
-      $('.weather-icon').html("<p>" + response.weather[0].icon);
-      $('.temperature-value').html("Temp: " + response.main.temp + '°F');
-      $('.humidity-value').html("Humidity: " + response.main.humidity + '%');
-      $('.wind-speed').html("Wind Speed: " + response.wind.speed + ' MPH');
+      $(".weather-icon").html("<p>" + response.weather[0].icon);
+      $(".temperature-value").html("Temp: " + response.main.temp + "°F");
+      $(".humidity-value").html("Humidity: " + response.main.humidity + "%");
+      $(".wind-speed").html("Wind Speed: " + response.wind.speed + " MPH");
+    })
 
-     
+  //Boston AP
+  $(".boston").on("click", event);
 
-    });
+  function event() {
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?" + "q=Boston&units=imperial&appid=" + APIKey;
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+      .then(function (response) {
 
-//need to find weather API to get temperature of several locations
-//display the weather retrieved from the API (city, date, icon image of weather, temperature, humidity, wind 
-//speed and uv index)
-//Include 5 day forecast below current weather to include (Date, icon image, temp and humidity)
-//Include a search history (local storage)
-//Will need convert temp from kelvin to farenheight
-//OpenWeatherMap API OpenWeather API
-//Mult functions needed to create different parts of dashboard (current conditions, 5-day forecact, search history
-//and UV index)
-//Will need nore than one ajax call
+        $(".location").html("<p>" + response.name);
+        $(".weather-icon").html("<p>" + response.weather[0].icon);
+        $(".temperature-value").html("Temp: " + response.main.temp + "°F");
+        $(".humidity-value").html("Humidity: " + response.main.humidity + "%");
+        $(".wind-speed").html("Wind Speed: " + response.wind.speed + " MPH");
 
-//Bonus use Geolocation to add user's current location to the initial landing page
+        //        console.log(queryURL)
+        //       console.log(response)
+      })
+
+    //Denver API
+    $(".denver").on("click", event);
+
+    function event() {
+      var queryURL = "https://api.openweathermap.org/data/2.5/weather?" + "q=Denver&units=imperial&appid=" + APIKey;
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      })
+        .then(function (response) {
+
+          $(".location").html("<p>" + response.name);
+          $(".weather-icon").html("<p>" + response.weather[0].icon);
+          $(".temperature-value").html("Temp: " + response.main.temp + "°F");
+          $(".humidity-value").html("Humidity: " + response.main.humidity + "%");
+          $(".wind-speed").html("Wind Speed: " + response.wind.speed + " MPH");
+
+          console.log(queryURL)
+          console.log(response)
+        })
+        
+        //Los Angeles API
+     $(".los-angeles").on("click", event);
+
+    function event() {
+   var queryURL = "https://api.openweathermap.org/data/2.5/weather?" + "q=Los Angeles&units=imperial&appid=" + APIKey;
+    $.ajax({
+     url: queryURL,
+   method: "GET"
+    })
+      .then(function (response) {
+
+    $(".location").html("<p>" + response.name);
+     $(".weather-icon").html("<p>" + response.weather[0].icon);
+       $(".temperature-value").html("Temp: " + response.main.temp + "°F");
+     $(".humidity-value").html("Humidity: " + response.main.humidity + "%");
+      })
+    }
+  }
+}
+}
+
+    
+  
+
